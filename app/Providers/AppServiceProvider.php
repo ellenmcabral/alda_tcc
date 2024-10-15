5966110589a36c9b2ea66765dc1be20f10d48308
Ellen Cabral <ellensilva.pl085@academico.ifsul.edu.br>
2024-10-15 14:43:15 -0300

    heroku deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Repositories\EloquentProductRepository;
 use App\Repositories\Interfaces\ProductRepositoryInterface;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if(app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
